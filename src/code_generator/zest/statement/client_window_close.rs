cb9206cc2bdfdd6541082d308ded6b8e1cfe5e7c
Pamplemousse <xav.maso@gmail.com>
2020-05-02 10:01:19 -0700

    Zest: Instantiate `ClientWindowClose` from parameters
      * with a `window_handle` to close the right window
      * with an `index` for the caller to ensure it to be
      run after all other statements

--- src/code_generator/zest/statement/client_window_close.rs
+++ src/code_generator/zest/statement/client_window_close.rs
@@ -1,12 +1,23 @@
 use serde::Serialize;
 
-#[derive(Clone, Serialize)]
+#[derive(Clone, Debug, PartialEq, Serialize)]
 #[serde(rename_all="camelCase")]
 pub struct ClientWindowCloseStatement {
     pub enabled: bool,
     pub index: usize,
     pub sleep_in_seconds: u32,
     pub window_handle: String,
+}
+
+impl ClientWindowCloseStatement {
+    pub fn from_index_and_window_handle(index: usize, window_handle: String) -> Self {
+        Self {
+            enabled: true,
+            index,
+            sleep_in_seconds: 0,
+            window_handle,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -38,5 +49,25 @@ mod tests {
             serde_json::from_str(generated_statement).unwrap(),
             serde_json::from_str(expected_result).unwrap()
         );
+    }
+
+    #[test]
+    fn instantiate_from_an_index_and_a_window_handle() {
+        let index = 100;
+        let window_handle = "a-handle".to_string();
+
+        let statement = ClientWindowCloseStatement::from_index_and_window_handle(
+            index,
+            window_handle,
+        );
+
+        let expected_statement = ClientWindowCloseStatement {
+            enabled: true,
+            index: 100,
+            sleep_in_seconds: 0,
+            window_handle: "a-handle".to_string(),
+        };
+
+        assert_eq!(statement, expected_statement);
     }
 }
